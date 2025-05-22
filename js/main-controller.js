390172308aa4452c83c5e1cd5ebb3ead6b77b2c5
Your Name <dan.weibren@gmail.com>
2025-05-22 08:48:43 +0300

    Add main-controller file with onInit function

---
+++ js/main-controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// LIST
+// להכין סרוויס וקונטרולר לכל ישות בנפרד
+
+// function onInit() {
+//     renderVideos()
+//     onRenderWiki()
+// }
+
+// // CREATE
+
+// // READ
+
+// function onSearchVideo() {
+
+// }
+
+// UPDATE
+
+// DELETE
+
