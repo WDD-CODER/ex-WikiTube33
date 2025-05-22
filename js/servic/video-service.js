d0629438ee2ad42630097b91880bdf8e74fe06cb
Your Name <dan.weibren@gmail.com>
2025-05-22 10:35:25 +0300

    Add api keys cache keys and global variables

--- js/servic/video-service.js
+++ js/servic/video-service.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const YOUTUBE_KEY = 'AIzaSyAI1_lARUYgn0v1k21qP4EMYDRtlsgx_10'
+const G_CACHE_KEY = 'gCacheDB'
+
+var gCache = loadFromStorage(G_CACHE_KEY) || null
+var gVideo = null
+
+
 // LIST
 // CREATE
 // READ
