4861aa8bb2aac1330c76d0650a3153ed789b044d
Your Name <dan.weibren@gmail.com>
2025-05-22 09:25:33 +0300

    Create render preview video function.

--- js/video-controller.js
+++ js/video-controller.js
@@ -12,6 +12,26 @@ function renderVideos(str) {
         .catch(err => console.log(err))
 }
 
+function renderPreviewVideos(ans) {
+    var res = ans.map((video, idx) => {
+        if (video.searchValue) {
+        const strHTML = `
+             <div class="video-card" id="video${idx}">
+                <figure>
+                    <img onload="onShowVideoCard(${idx})" class="video-img" src="${video.imgUrl}" alt="video image">
+                </figure>
+                <div class="video-info">
+                  <h4 class="video-title">${video.videoTitle}</h4>  
+                  <time data-time="${video.dateOfPublish}" class="video-publishTime">${video.dateOfPublish}</time>
+                  </div>
+                  <p class="description">${video.description}</p>
+            </div>
+ `
+        return strHTML
+        }
+    })
+    document.querySelector('.video-list').innerHTML = res.join('')
+}
 
 // CREATE
 
