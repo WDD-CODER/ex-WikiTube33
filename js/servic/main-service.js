b23efb037f9c1f6f0878cf25431e62831f0da198
Your Name <dan.weibren@gmail.com>
2025-05-22 09:59:25 +0300

    Add updateGCache function

--- js/servic/main-service.js
+++ js/servic/main-service.js
@@ -2,6 +2,17 @@
 
 // LIST
 // CREATE
+function updateGCache(data) {
+    const parameter = data.searchTitle ? 'articleTitle' : 'videoTitle'
+    if (!getGCache()) gCache = []
+
+    data.forEach(newItem => {
+        const exist = gCache.some(item => item[parameter] === newItem[parameter])
+        if (!exist) gCache.push(newItem)
+    })
+    return gCache
+}
+
 // READ
 // UPDATE
 // DELETE
