fc03ec1314607582dd9ec00e411483a0deb3c771
Your Name <dan.weibren@gmail.com>
2025-05-22 10:37:23 +0300

    Add onAskVideos function api call for youTube

--- js/servic/video-service.js
+++ js/servic/video-service.js
@@ -6,8 +6,32 @@ const G_CACHE_KEY = 'gCacheDB'
 var gCache = loadFromStorage(G_CACHE_KEY) || null
 var gVideo = null
 
-
 // LIST
+
+function onAskVideos(str) {
+    const searchValue = str || 'Music videos'
+    gVideo = searchValue
+    if (isVideoInCache(searchValue)) {
+        console.log('from cache')
+        // return gCache
+        return Promise.resolve(gCache)
+    }
+
+    const YOUTUBE_OPTIONS5_KEY = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_KEY}&q=${searchValue}`
+    return axios.get(YOUTUBE_OPTIONS5_KEY)
+        .then(res => {
+            console.log('from Ajax')
+            return res.data.items
+        })
+        .then(ans => {
+            let formattedAns = formattedVideoData(ans, searchValue)
+            updateGCache(formattedAns)
+            saveToStorage(G_CACHE_KEY, gCache)
+        })
+        .catch(() => console.log('loading from the api failed.'))
+
+}
+
 // CREATE
 // READ
 // UPDATE
