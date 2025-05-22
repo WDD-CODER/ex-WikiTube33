9c8ee746049d44f138ed6121af63d7416dcbc97a
Your Name <dan.weibren@gmail.com>
2025-05-22 12:27:26 +0300

    General code refactoring: moved onRenderWiki from main controller into internal function renderVideos.

--- js/servic/main-service.js
+++ js/servic/main-service.js
@@ -3,12 +3,14 @@
 // LIST
 // CREATE
 function updateGCache(data) {
+    console.log("🚀 ~ updateGCache ~ data:", data)
     const parameter = data[0].searchTitle ? 'articleTitle' : 'videoTitle'
     if (!getGCache()) gCache = []
     data.forEach(newItem => {
         const exist = gCache.some(item => item[parameter] === newItem[parameter])
         if (!exist) gCache.push(newItem)
     })
+    console.log("🚀 ~ updateGCache ~ gCache:", gCache)
     return gCache
 }
 
